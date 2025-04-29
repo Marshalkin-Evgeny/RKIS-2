36e556758ed1d7297148bab02fdf2f6537661323
Git-art-dark <vladimirrumancev27544@gmail.com>
2025-04-29 13:33:42 +0300

    lesson 8: make chik-chirik

--- Занятие 8/chik-chirik/Models/User.cs
+++ Занятие 8/chik-chirik/Models/User.cs
@@ -1,6 +1,10 @@
 ﻿namespace chik_chirik.Models
 {
-	public class User
-	{
-	}
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } // JSONPlaceholder uses 'username' instead of 'name'
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
 }
